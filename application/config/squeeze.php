c2af4c4cc478b794330f4a6402a80c9cd604becd
Munir Ahmad <munir.ehmad@gmail.com>
2012-02-28 12:37:39 +0500

    organized for CI

---
+++ application/config/squeeze.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['minify_base'] = 'addons/min/'; // Include the trailing slash in ONLY THIS
+
+$config['js_base'] = 'loop_new/site/js';
+$config['css_base'] = 'loop_new/site/styles';
+
+
+?>
